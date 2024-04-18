a0dc6b914c32aca70a1898ff8f43c8d25adf6342
Shivam-Samant <shivamsamant000@gmail.com>
2024-04-18 16:18:06 +0530

    input text color fixes

--- components/main/contact.tsx
+++ components/main/contact.tsx
@@ -31,15 +31,15 @@ export const ContactMe = () => {
             <form onSubmit={sendEmail} className="contact-form space-y-8">
                 <div>
                     <label htmlFor="user_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
-                    <input style={{ background: "hsla(0, 0%, 100%, .1)" }} ref={nameRef} type="text" name="user_name" className="block p-3 w-full text-sm text-gray-900 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Your name" required />
+                    <input style={{ background: "hsla(0, 0%, 100%, .1)" }} ref={nameRef} type="text" name="user_name" className="block p-3 w-full text-sm text-gray-300 dark:text-gray-300 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Your name" required />
                 </div>
                 <div>
                     <label htmlFor="user_email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-                    <input style={{ background: "hsla(0, 0%, 100%, .1)" }} ref={emailRef} type="email" name="user_email" className="shadow-sm border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="example@gmail.com" required />
+                    <input style={{ background: "hsla(0, 0%, 100%, .1)" }} ref={emailRef} type="email" name="user_email" className="shadow-sm border border-gray-300 text-gray-300 dark:text-gray-300 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="example@gmail.com" required />
                 </div>
                 <div className="sm:col-span-2">
                     <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your message</label>
-                    <textarea style={{ background: "hsla(0, 0%, 100%, .1)" }} ref={messageRef} name="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Message..."></textarea>
+                    <textarea style={{ background: "hsla(0, 0%, 100%, .1)" }} ref={messageRef} name="message" rows={6} className="block p-2.5 w-full text-sm text-gray-300 dark:text-gray-300 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Message..."></textarea>
                 </div>
                 <button type="submit" className="px-4 py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]">Send Message</button>
             </form>
